4670938e06a04a8c08faa245b0a76e8bf3df8f82
Ashwin Sekar <ashwin@solana.com>
2021-09-14 22:18:09 -0400

    Some progress on creating tower during blockstore processing

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1363,7 +1363,7 @@ impl Bank {
     }
 
     /// Returns all ancestors excluding self.slot.
-    pub(crate) fn proper_ancestors(&self) -> impl Iterator<Item = Slot> + '_ {
+    pub fn proper_ancestors(&self) -> impl Iterator<Item = Slot> + '_ {
         self.ancestors
             .keys()
             .into_iter()
