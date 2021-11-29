00128195aa85e8d3945890208f130c203978cf4f
Ashwin Sekar <ashwin@solana.com>
2021-11-28 18:26:21 -0800

    Revert "Preserve pre-existing vote/stake keypairs for bootstrap validator (#19405)"
    This reverts commit d5594263730ae3164af8c1f10f0cf5d3f4864d3e.

--- multinode-demo/setup.sh
+++ multinode-demo/setup.sh
@@ -21,16 +21,9 @@ if [[ -f $BOOTSTRAP_VALIDATOR_IDENTITY_KEYPAIR ]]; then
 else
   $solana_keygen new --no-passphrase -so "$SOLANA_CONFIG_DIR"/bootstrap-validator/identity.json
 fi
-if [[ -f $BOOTSTRAP_VALIDATOR_STAKE_KEYPAIR ]]; then
-  cp -f "$BOOTSTRAP_VALIDATOR_STAKE_KEYPAIR" "$SOLANA_CONFIG_DIR"/bootstrap-validator/stake-account.json
-else
-  $solana_keygen new --no-passphrase -so "$SOLANA_CONFIG_DIR"/bootstrap-validator/stake-account.json
-fi
-if [[ -f $BOOTSTRAP_VALIDATOR_VOTE_KEYPAIR ]]; then
-  cp -f "$BOOTSTRAP_VALIDATOR_VOTE_KEYPAIR" "$SOLANA_CONFIG_DIR"/bootstrap-validator/vote-account.json
-else
-  $solana_keygen new --no-passphrase -so "$SOLANA_CONFIG_DIR"/bootstrap-validator/vote-account.json
-fi
+
+$solana_keygen new --no-passphrase -so "$SOLANA_CONFIG_DIR"/bootstrap-validator/vote-account.json
+$solana_keygen new --no-passphrase -so "$SOLANA_CONFIG_DIR"/bootstrap-validator/stake-account.json
 
 args=(
   "$@"
